c6159153446bde20e4502ce45cc775a4ba3ea5a4
orknytr-fb <78570796+orknytr-fb@users.noreply.github.com>
2021-02-26 02:52:09 +0300

    haydii

---
+++ TesterProject/TesterProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TesterProject
+//
+//  Created by Orkun on 26.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("shaktar")
+        
+        
+    }
+
+
+}
+
